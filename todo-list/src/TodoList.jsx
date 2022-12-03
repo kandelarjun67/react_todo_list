606e669beceaff65e445ca840ffef7488397d97d
kandelarjun67 <kandelarjun67@gmail.com>
2022-12-03 12:17:00 +0545

    add and delete done in todo

--- todo-list/src/TodoList.jsx
+++ todo-list/src/TodoList.jsx
@@ -15,26 +15,50 @@ export default function TodoList() {
             setActivity(' ')
         }
     }
+
+    const removeTheItem = (i)=> {
+        // const upDatedList = [...list]
+
+       const upDatedList = list.filter((elem, id) => {
+          return i !== id
+        })
+        setList(upDatedList)
+    }
+
+const removeAll = () => {
+    setList([])
+}
     return (
         <>
-            <div className="container bg-light p-5">
+            <div className="container bg-light py-5">
                 <div className="row">
                     <div className="col-lg-6">
                         <input type="text" placeholder='Add Activity' className='form-control py-3' value={activity} onChange={(e) => setActivity(e.target.value)} />
                     </div>
                     <div className="col-lg-4">
-                        <button className='btn btn-info btn-lg py-3 px-5' onClick={btnClicked}>Add</button>
+                        <button className='btn btn-primary btn-lg py-3 px-5' onClick={btnClicked}>Add</button>
                     </div>
                 </div>
                 </div>
                 <div className="container bg-dark text-light py-3">
              
                 <h3>Here is the List :{")"}</h3>
-                {list.map((item) =>
-                    <li>
+                {list.map((item,i) =>
+                <><div className="row ">
+                    <div className="col-lg-6">
+                    <p className='list-data'>
                         {item}
-                    </li>
+                    </p>
+                    </div>
+                   <div className="col-lg-6">
+                   <button className='btn btn-danger btn-lg' onClick={() =>removeTheItem(i)}>Remove</button>
+                   </div>
+                   </div>
+                   
+                </>
+
                 )}
+                {list.length >= 1 && <button className='btn btn-danger btn-lg' onClick={removeAll}>Remove All</button>}
                        
                        </div>
 
